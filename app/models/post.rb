918a0bb06a36413b1ef2e923150e10fa91b26c1d
Masato Ikeda <masato.ikeda@gmail.com>
2011-09-21 15:00:00 +0900

    add validation before posting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   scope :newer, order("created_at DESC")
 
+  validates_presence_of :name
+  validates_presence_of :body
+
   def new?
     Time.now - created_at < 6*60*60
   end
